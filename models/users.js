b4189acb311e1b77f27f9ca43ba23f0398975d99
yuda-lyu <firsemisphere@gmail.com>
2022-04-12 15:43:53 +0800

    modify: msg

--- models/users.js
+++ models/users.js
@@ -1,26 +1,26 @@
 /* jshint indent: 1 */
 module.exports = function(sequelize, DataTypes) {
-	return sequelize.define('users', {
-		'id': {
-			type: DataTypes.STRING,
-			allowNull: false,
-			primaryKey: true,
-			autoIncrement: false,
-			comment: null
-		},
-		'name': {
-			type: DataTypes.TEXT,
-			allowNull: true,
-			autoIncrement: false,
-			comment: null
-		},
-		'value': {
-			type: DataTypes.FLOAT,
-			allowNull: true,
-			autoIncrement: false,
-			comment: null
-		}
-	}, {
-		tableName: 'users'
-	});
+return sequelize.define('users', {
+'id': {
+type: DataTypes.STRING,
+allowNull: false,
+primaryKey: true,
+autoIncrement: false,
+comment: null
+},
+'name': {
+type: DataTypes.TEXT,
+allowNull: true,
+autoIncrement: false,
+comment: null
+},
+'value': {
+type: DataTypes.FLOAT,
+allowNull: true,
+autoIncrement: false,
+comment: null
+}
+}, {
+tableName: 'users'
+});
 };
